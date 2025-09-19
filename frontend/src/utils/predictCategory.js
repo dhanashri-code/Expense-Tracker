aee2133a3b71d52af4b7cb6503eb6c7f40eff693
Dhanashri Bhavsar <129056367+dhanashri-code@users.noreply.github.com>
2025-09-19 22:21:08 +0530

    Update predictCategory.js

--- frontend/src/utils/predictCategory.js
+++ frontend/src/utils/predictCategory.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const predictCategory = async (title) => {
   try {
-    const response = await axios.post('http://localhost:5000/api/predict', { title });
+    const response = await axios.post('https://expense-tracker-1-pjm7.onrender.com/api/predict', { title });
     return response.data.predictedCategory || 'Other';
   } catch (error) {
     console.error("Prediction API error:", error.message);
